a86709d4adaaf63242d6632be56d3d15a52761a5
Santhosh <santhosh@flyweightsoft.com>
2026-08-01 11:27:44 +0530

    feat: add portfolio page and refactor global layout styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
   },
   {
     path: 'custom-explorer-sort',
-    loadComponent: () => import('./pages/custom-explorer-sort/custom-explorer-sort.component').then(m => m.CustomExplorerSortComponent)
+    loadComponent: () => import('./pages/custom-explorer-sort/custom-explorer-sort.component').then(c => c.CustomExplorerSortComponent)
   },
   {
     path: 'k8sdesk',
-    loadComponent: () => import('./pages/k8sdesk/k8sdesk.component').then(m => m.K8sdeskComponent)
+    loadComponent: () => import('./pages/k8sdesk/k8sdesk.component').then(c => c.K8sdeskComponent)
+  },
+  {
+    path: 'portfolio',
+    loadComponent: () => import('./pages/portfolio/portfolio.component').then(c => c.PortfolioComponent)
   },
   {
     path: '**',
